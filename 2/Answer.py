78da0162cad0ef8756ecab049a49c9f89402bd85
MAHESH-THILAK-K <majesticmaheshthilak@gmail.com>
2026-03-03 15:34:41 +0530

    If else ANSWER

--- 2/Answer.py
+++ 2/Answer.py
@@ -1,1 +1,30 @@
+#!/bin/python3
 
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+    # Use try-except if you suspect input issues, 
+    # but the standard way is:
+    try:
+        line = sys.stdin.read().strip()
+        if line:
+            n = int(line)
+        else:
+            sys.exit(0)
+    except EOFError:
+        sys.exit(0)
+
+    # Core Logic
+    if n % 2 != 0:
+        print("Weird")
+    else:
+        if 2 <= n <= 5:
+            print("Not Weird")
+        elif 6 <= n <= 20:
+            print("Weird")
+        elif n > 20:
+            print("Not Weird")
